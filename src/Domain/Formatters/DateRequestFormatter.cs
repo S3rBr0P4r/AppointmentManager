2ad9b8fd32d72b90ee7a83440aefe5baf46e33b6
Sergio Brotons <s3rbr0p4r@gmail.com>
2025-05-14 21:23:37 +0200

    Formatter to have date compatible with slot service added

---
+++ src/Domain/Formatters/DateRequestFormatter.cs
@@ -0,0 +1,26 @@
+﻿namespace AppointmentManager.Domain.Formatters
+{
+    public class DateRequestFormatter : IDateRequestFormatter
+    {
+        private const int October = 10;
+
+        public string GetCompatibleDateWithSlotService(DateOnly date)
+        {
+            var compatibleDate = date.Year.ToString();
+            compatibleDate += GetMonth(date.Month);
+            compatibleDate += GetDay(date.Day);
+
+            return compatibleDate;
+        }
+
+        private static string GetMonth(int monthNumber)
+        {
+            return monthNumber < October ? $"0{monthNumber}" : monthNumber.ToString();
+        }
+
+        private static string GetDay(int dayNumber)
+        {
+            return dayNumber < 10 ? $"0{dayNumber}" : dayNumber.ToString();
+        }
+    }
+}
---
+++ tests/Domain/Formatters/DateRequestFormatterShould.cs
@@ -0,0 +1,29 @@
+﻿using AppointmentManager.Domain.Formatters;
+
+namespace AppointmentManager.Domain.Tests.Formatters
+{
+    public class DateRequestFormatterShould
+    {
+        [Theory]
+        [MemberData(nameof(Dates))]
+        public void ReturnValidDateForSlotServiceEndpoint(DateOnly date, string expectedDateForEndpoint)
+        {
+            // Arrange
+            var sut = new DateRequestFormatter();
+
+            // Act
+            var compatibleDate = sut.GetCompatibleDateWithSlotService(date);
+
+            // Assert
+            Assert.Equal(expectedDateForEndpoint, compatibleDate);
+        }
+
+        public static IEnumerable<object[]> Dates =>
+        [
+            [new DateOnly(2025, 1, 2), "20250102"],
+            [new DateOnly(2025, 10, 2), "20251002"],
+            [new DateOnly(2025, 8, 20), "20250820"],
+            [new DateOnly(2025, 12, 12), "20251212"]
+        ];
+    }
+}
