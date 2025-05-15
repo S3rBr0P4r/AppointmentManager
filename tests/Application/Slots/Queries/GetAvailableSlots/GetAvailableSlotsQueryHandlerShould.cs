baa3191e5989c39a771b25a4df61c512b238db3a
Sergio Brotons <s3rbr0p4r@gmail.com>
2025-05-15 12:41:27 +0200

    Unit tests for application layer added

---
+++ tests/Application/Slots/Queries/GetAvailableSlots/GetAvailableSlotsQueryHandlerShould.cs
@@ -0,0 +1,25 @@
+﻿using AppointmentManager.Application.Slots.Queries.GetAvailableSlots;
+using AppointmentManager.Domain.Services;
+using Moq;
+using Xunit;
+
+namespace AppointmentManager.Application.Tests.Slots.Queries.GetAvailableSlots
+{
+    public class GetAvailableSlotsQueryHandlerShould
+    {
+        [Fact]
+        public async Task CallSlotsManagementService()
+        {
+            // Arrange
+            var query = new GetAvailableSlotsQuery(DateOnly.MaxValue);
+            var slotsManagementService = new Mock<ISlotsManagementService>();
+            var sut = new GetAvailableSlotsQueryHandler(slotsManagementService.Object);
+
+            // Act
+            await sut.Handle(query, CancellationToken.None);
+
+            // Assert
+            slotsManagementService.Verify(sms => sms.GetAvailableSlotsAsync(query.Date, CancellationToken.None), Times.Once);
+        }
+    }
+}
